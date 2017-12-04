1d3baef16f6b99af729b49318aa9950905dac99a
kazuhide hane <hidekkobonba1213@gmail.com>
2017-12-04 06:12:30 +0000

    addculumn name

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204060128) do
+ActiveRecord::Schema.define(version: 20171204061054) do
 
   create_table "messages", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
